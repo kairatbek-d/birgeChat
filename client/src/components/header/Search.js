ca9bf080b811baaa9e7e7816b57055a2a0dc0a1c
KaIRaTBeK <45318061+kairatbek-d@users.noreply.github.com>
2024-02-08 10:04:57 -0800

    profile

--- client/src/components/header/Search.js
+++ client/src/components/header/Search.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux';
 import { getDataAPI } from '../../utils/fetchData';
 import { GLOBALTYPES } from '../../redux/actions/globalTypes';
-import { Link } from 'react-router-dom';
 import UserCard from '../UserCard';
+import LoadIcon from '../../images/loading.gif'
 
 const Search = () => {
     const [search, setSearch] = useState('')
@@ -11,21 +11,23 @@ const Search = () => {
 
     const auth = useSelector(state => state.auth)
     const dispatch = useDispatch()
+    const [load, setLoad] = useState(false)
 
-    useEffect(() => {
-        if(search) {
-            getDataAPI(`search?username=${search}`, auth.token)
-            .then(res => setUsers(res.data.users))
-            .catch(err => {
-                dispatch({
-                    type: GLOBALTYPES.ALERT, payload: {error: err.response.data.msg}
-                })
+    const handleSearch = async (e) => {
+        e.preventDefault()
+        if(!search) return;
+
+        try {
+            setLoad(true)
+            const res = await getDataAPI(`search?username=${search}`, auth.token)
+            setUsers(res.data.users)
+            setLoad(false)
+        } catch (err) {
+            dispatch({
+                type: GLOBALTYPES.ALERT, payload: {error: err.response.data.msg}
             })
-        } else {
-            setUsers([])
         }
-
-    }, [search, auth.token, dispatch])
+    }
 
     const handleClose = () => {
         setSearch('')
@@ -33,7 +35,7 @@ const Search = () => {
     }
 
     return (
-        <form className="search_form">
+        <form className="search_form" onSubmit={handleSearch}>
             <input type="text" name="search" value={search} id="search"
             onChange={e => setSearch(e.target.value.toLowerCase().replace(/ /g, ''))} />
 
@@ -47,12 +49,18 @@ const Search = () => {
                 &times;
             </div>
 
+            <button type="submit" style={{display: 'none'}}>Search</button>
+
+            {load && <img className="loading" src={LoadIcon} alt="loading" />}
+
             <div className="users">
                 {
                     search && users.map(user => (
-                        <Link key={user._id} to={`/profile/${user._id}`} onClick={handleClose}>
-                            <UserCard user={user} border="border" />
-                        </Link>
+                        <UserCard
+                        key={user._id} 
+                        user={user}
+                        border="border"
+                        handleClose={handleClose} />
                     ))
                 }
             </div>
--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -5,8 +5,18 @@ const userCtrl = {
         try {
             const users = await Users.find({username: {$regex: req.query.username}})
             .limit(10).select("fullname username avatar")
-
+            console.log(req)
             res.json({users})
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    getUser: async (req, res) => {
+        try {
+            const user = await Users.findById(req.params.id).select("-password")
+            if(!user) return res.status(400).json({msg: "User does not exist."})
+            
+            res.json({user})
         } catch (err) {
             return res.status(500).json({msg: err.message})
         }
